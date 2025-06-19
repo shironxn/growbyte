da0cf53e2fd75a87a138b7b135d7c48469a3faa6
shironxn <shironxn@gmail.com>
2025-06-19 09:20:39 +0700

    chore: update pin components

--- include/Config.h
+++ include/Config.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define DHT_PIN 0
-#define DS18B20_PIN 0
-#define SOIL_PIN 0
+#define DHT_PIN 34
+#define DS18B20_PIN 2
+#define SOIL_PIN 35
 
-#define PUMP_RELAY_PIN 0
-#define LIGHT_RELAY_PIN 0
+#define PUMP_RELAY_PIN 27
+#define LIGHT_RELAY_PIN 26

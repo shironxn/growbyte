4bcb8f157a2c461467959ab6f3841fb3acec1b87
shironxn <shironxn@gmail.com>
2025-07-15 14:09:07 +0700

    chore: update style and pinout sensor

--- include/Config.h
+++ include/Config.h
@@ -1,15 +1,15 @@
 #pragma once
 
-#define DHT_PIN 4
-#define DS18B20_PIN 2
-#define SOIL_PIN 32
+#define DHT_PIN         21
+#define DS18B20_PIN     19
+#define SOIL_PIN        34
 
-#define PUMP_RELAY_PIN 27
-#define LIGHT_RELAY_PIN 26
+#define PUMP_RELAY_PIN  26
+#define LIGHT_RELAY_PIN 27
 
-#define LCD_ADDRESS 0x27
-#define LCD_COLUMNS 16
-#define LCD_ROWS 2
+#define LCD_ADDRESS     0x27
+#define LCD_COLUMNS     16
+#define LCD_ROWS        2
 
-#define SOIL_THRESHOLD 30
-#define TEMP_THRESHOLD 25
+#define SOIL_THRESHOLD  30
+#define TEMP_THRESHOLD  25
